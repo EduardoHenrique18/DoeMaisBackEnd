41bbdbb15fb8dc784cb84f736d4c239887728cfc
Eduardo Henrique Araujo Bezerra <e.araujo.bezerra@avanade.com>
2020-04-16 22:27:57 -0300

    modify HttpResponse.badRequest to receive an error

--- src/presentation/helpers/http-response.js
+++ src/presentation/helpers/http-response.js
@@ -1,12 +1,11 @@
-const MissingParamError = require('./missing-param-error')
 const UnauthorizedError = require('./unauthorizedError')
 const ServerError = require('./server-error')
 
 module.exports = class httpResponse {
-  static badRequest (paramName) {
+  static badRequest (error) {
     return {
       statusCode: 400,
-      body: new MissingParamError(paramName)
+      body: error
     }
   }
 
