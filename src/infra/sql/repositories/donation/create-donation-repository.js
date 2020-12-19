12a83f11bc50f00deec96b53136617e8b936c8ab
Jaciel <jacielferreira1994@hotmail.com>
2020-12-18 22:23:29 -0300

    alteração nos repositories de donation

--- src/infra/sql/repositories/donation/create-donation-repository.js
+++ src/infra/sql/repositories/donation/create-donation-repository.js
@@ -2,18 +2,11 @@ const Donation = require('../../entities-db/donation')
 const ServerError = require('../../../../utils/errors/server-error')
 
 module.exports = class CreateDonationRepository {
-  CreateDonation (donation) {
+  async CreateDonation (donation) {
     try {
       const { name, description, userId, image, pointId, isPublic } = donation
 
-      return Donation.create({
-        name,
-        pointId,
-        isPublic,
-        description,
-        image,
-        userId
-      })
+      return await Donation.create({ name, description, userId, image, pointId, isPublic })
     } catch (err) {
       throw new ServerError()
     }
--- src/infra/sql/repositories/donation/read-donation-repository.js
+++ src/infra/sql/repositories/donation/read-donation-repository.js
@@ -1,15 +1,10 @@
 const Donation = require('../../entities-db/donation')
 const ServerError = require('../../../../utils/errors/server-error')
-const Point = require('../../entities-db/point')
 
 module.exports = class ReadDonationRepository {
   async ReadDonationByPointId (pointId) {
     try {
-      const results = await Point.sequelize.query(`SELECT * FROM donations as donations
-                                                INNER JOIN users ON donations.userId = users.userId 
-                                                WHERE donations.pointId = ${pointId} AND
-                                                donations.isPublic = true`)
-      return results[0]
+      return await Donation.findById({ pointId })
     } catch (err) {
       console.log(err)
       throw new ServerError()
@@ -20,12 +15,7 @@ module.exports = class ReadDonationRepository {
     try {
       const { image } = donation
 
-      const read = await Donation.findAll({
-        where: {
-          image
-        }
-      })
-      return read
+      return await Donation.findOne({ image })
     } catch (err) {
       throw new ServerError()
     }
