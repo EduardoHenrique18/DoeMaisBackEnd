44cf38cd9bb7369f1ab569341b38d37895955ef7
Jaciel <jacielferreira1994@hotmail.com>
2020-12-18 22:22:50 -0300

    alteração nas rotas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-const App = require('./infra/express/app')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-App.listen(8080)
+const routes = require('./Routes');
+
+const app = express();
+
+app.use(cors());
+
+var uri = process.env.ATLAS_URI||'mongodb://edu:1123581321@cluster0-shard-00-00.ubafb.mongodb.net:27017,cluster0-shard-00-01.ubafb.mongodb.net:27017,cluster0-shard-00-02.ubafb.mongodb.net:27017/test?ssl=true&replicaSet=atlas-fwpg8v-shard-0&authSource=admin&retryWrites=true&w=majority'
+var PORT = process.env.PORT||'8080'
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }).catch(err => console.log(err));
+
+app.use(express.json({ limit: '50mb' }));
+
+app.use(routes);
+
+app.listen(PORT, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
